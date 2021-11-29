126a880c3efbb65f8a49139c060447a3c0569666
Krikal <krikal.csaladi@gmail.com>
2021-11-29 17:59:05 +0100

    style: add gun images

--- app/src/main/java/com/example/gunapplication/MainActivity.java
+++ app/src/main/java/com/example/gunapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.gunapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView headerTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Title*/
+        headerTitle = (TextView) findViewById(R.id.header_title);
+        headerTitle.setText("Fegyver applikáció");
+
+
+
     }
 }
